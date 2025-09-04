f1f3ba2449d65a2923bbe33166c3756c2c1b3737
Shubham Jumde <shubham.jumde@interpersonal.work>
2025-09-03 23:02:09 -0500

    Final fixes to the file

--- shaders/LuminosityHighPassShader.js
+++ shaders/LuminosityHighPassShader.js
@@ -22,7 +22,7 @@ const LuminosityHighPassShader = {
 		'tDiffuse': { value: null },
 		'luminosityThreshold': { value: 1.0 },
 		'smoothWidth': { value: 1.0 },
-		'defaultColor': { value: new Color( 0xFFFFFF ) },
+		'defaultColor': { value: new Color( 0x000000 ) },
 		'defaultOpacity': { value: 1.0 }
 
 	},
